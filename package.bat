3919a1e74fba00760fa3e823b13d2b80a98477e8
daitenshionyan <daitenshionyan@gmail.com>
2023-09-10 17:02:13 +0800

    Update package.bat
    To not require arguments to run.

--- package.bat
+++ package.bat
@@ -1,39 +1,79 @@
-ECHO OFF
-SET name=%1
-SET binDir=%2
-SET srcDir=%3
-SET outDir=%4
+@echo off
+SETLOCAL DISABLEDELAYEDEXPANSION
 
-SET binDir=%binDir:/=\%
-SET srcDir=%srcDir:/=\%
-SET outDir=%outDir:/=\%
+set SRC_PATH=%~dp0
 
-SET zipper="C:\Program Files\7-Zip\7z.exe"
+set EXE_NAME_VAR_NAME=VRFBCALC_EXECUTABLE_NAME
+set EXE_OUTP_VAR_NAME=EXECUTABLE_OUTPUT_PATH
 
-ECHO ---- [ PACKAGING ] -------------------------------------------------------------
-IF EXIST "%outDir%\%name%.zip" (
-    ECHO Deleting archive: %outDir%\%name%.zip
-    DEL "%outDir%\%name%.zip"
+set CMAKE_CACHE_PATH=%SRC_PATH%build\CMakeCache.txt
+set ZIPPER=C:\Program Files\7-Zip\7z.exe
+
+if not exist "%ZIPPER%" (
+    echo [91m[ERROR][0m Cannot find 7z executable - %ZIPPER%
+    exit 1
+)
+if not exist "%CMAKE_CACHE_PATH%" (
+    echo [91m[ERROR][0m Cannot find CMakeCache.txt file - %CMAKE_CACHE_PATH%
+    exit 1
 )
 
-IF EXIST %zipper% (
-    CD "%binDir%"
-    %zipper% a -tzip "%outDir%\%name%.zip" "%name%.exe" ^
-        | findstr ^
-            /c:"Creating archive" ^
-            /c:"Add new data to archive:" ^
-        || GOTO PACK_END
-    %zipper% a -tzip "-x!*.exe" "-xr!output" "%outDir%\%name%.zip" * ^
-        | findstr ^
-            /c:"Add new data to archive:" ^
-        || GOTO PACK_END
-    CD "%srcDir%"
-    %zipper% a -tzip "%outDir%\%name%.zip" COPYING COPYING.LESSER README.md ^
-        | findstr ^
-            /c:"Add new data to archive:" ^
-            /c:"Archive size" ^
-        || GOTO PACK_END
-) ELSE (
-    ECHO Failed to zip as %zipper% not found
+set EXE_NAME_FOUND=false
+set EXE_OUTP_FOUNT=false
+for /f "usebackq tokens=1 eol=/ delims=" %%G in ("%CMAKE_CACHE_PATH%") do (
+    for /f "usebackq tokens=1,2,* eol=# delims==:" %%H in ('%%~G') do (
+        if %%H==%EXE_NAME_VAR_NAME% (
+            set EXE_NAME_FOUND=true
+            set EXE_NAME=%%~J
+        )
+        if %%H==%EXE_OUTP_VAR_NAME% (
+            set EXE_OUTP_FOUND=true
+            set OUT_PATH=%%~J
+        )
+    )
 )
-:PACK_END
+
+if %EXE_NAME_FOUND%==false (
+    echo [91m[ERROR][0m %EXE_NAME_VAR_NAME% variable not found in CMakeCache.txt
+    exit 1
+)
+if %EXE_OUTP_FOUND%==false (
+    echo [91m[ERROR][0m %EXE_OUTP_VAR_NAME% variable not found in CMakeCache.txt
+    exit 1
+)
+
+set BIN_PATH=%OUT_PATH%\Release
+set EXE_PATH=%OUT_PATH%\Release\%EXE_NAME%.exe
+set PAK_PATH=%OUT_PATH%\package\%EXE_NAME%.zip
+
+if not exist "%BIN_PATH%" (
+    echo [91m[ERROR][0m Cannot find binary output folder - %BIN_PATH%
+    exit 1
+)
+if not exist "%EXE_PATH%" (
+    echo [91m[ERROR][0m Cannot find executable file - %EXE_PATH%
+    exit 1
+)
+if exist "%PAK_PATH%" (
+    del "%PAK_PATH%"
+    echo Deleted old package
+)
+
+echo Packaging executable...
+"%ZIPPER%" a -tzip "%PAK_PATH%" "%EXE_PATH%" ^
+    | findstr ^
+        /c:"Add new data to archive:"
+
+echo Packaging dependencies...
+"%ZIPPER%" a -tzip "-x!*.exe" "-x!output" "%PAK_PATH%" "%BIN_PATH%\*" ^
+    | findstr ^
+        /c:"Add new data to archive:"
+
+echo Packaging readme and license...
+"%ZIPPER%" a -tzip "%PAK_PATH%" "%SRC_PATH%\COPYING" "%SRC_PATH%\COPYING.LESSER" "%SRC_PATH%\README.md" ^
+    | findstr ^
+        /c:"Add new data to archive:" ^
+        /c:"Archive size"
+
+echo.
+echo [92m[SUCCESS][0m Packaging completed - %PAK_PATH%
