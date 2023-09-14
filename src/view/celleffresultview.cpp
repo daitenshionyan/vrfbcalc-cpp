3b13fd7ee3a728ea80ae5749d64435aade5e6ee5
daitenshionyan <daitenshionyan@gmail.com>
2023-09-14 16:37:18 +0800

    Add axis tick adjustments

--- src/view/celleffresultview.cpp
+++ src/view/celleffresultview.cpp
@@ -1,31 +1,89 @@
 #include "view/celleffresultview.h"
 #include "./ui_celleffresultview.h"
 
+#include <cmath>
 #include <exception>
 #include <filesystem>
 #include <vector>
 
 #include <QtCharts/QChartView>
 #include <QtCharts/QLineSeries>
+#include <QtCharts/QValueAxis>
 
 #include "driver/vrfbdriver_io.hpp"
 
 
-namespace {
+namespace { // BEGIN OF NAMESPACE <GLOBAL::UNNAMED> ============================
+
+
+constexpr int kIntervalFactor = 5;
+constexpr int kMaxHorizontalTick = 11;
+
+
+int calcInterval(int diff, int targetCount = kMaxHorizontalTick) {
+  int interval = (int) std::ceil((double) diff / (targetCount+1));
+  if (interval > kIntervalFactor) {
+    interval += interval % kIntervalFactor;
+  }
+  return interval;
+}
+
+
+QValueAxis* createAxisWhole(int min, int max) {
+  int diff = max - min;
+  int interval = calcInterval(diff);
+  int tickCount = (int) std::ceil((double) diff / interval) + 1;
+  int maxRange = min + interval * (tickCount-1);
+  QValueAxis* axis = new QValueAxis();
+  axis->setRange(min, maxRange);
+  axis->setTickInterval(interval);
+  axis->setTickCount(tickCount);
+  axis->setLabelFormat("%d");
+  return axis;
+}
+
+
+QValueAxis* createAxisDouble(double min, double max, int targetCount) {
+  double diff = max - min;
+  int pow = 0;
+  for (double num = diff; std::ceil(num) < targetCount-1; num *= 10) {
+    ++pow;
+  }
+  double interval = calcInterval((int) (diff * std::pow(10, pow)), targetCount)
+      / std::pow(10, pow);
+  int tickCount = (int) std::ceil(diff / interval) + 1;
+  double maxRange = min + interval * (tickCount-1);
+  QValueAxis* axis = new QValueAxis();
+  axis->setRange(min, maxRange);
+  axis->setTickInterval(interval);
+  axis->setTickCount(tickCount);
+  return axis;
+}
 
 
 QChartView* createChart_CE(const std::vector<vrfb::Table>& tables) {
   QChart* chart = new QChart();
+  std::size_t max_cyc_num = 0;
+  double max_ce = 0;
   for (const vrfb::Table& table : tables) {
     QLineSeries* series = new QLineSeries();
     for (std::size_t i = 0; i < table.numRows(); ++i) {
-      series->append(
-          table.get<int>("Cycle No.", i),
-          table.get<double>("CE (Fractional)", i));
+      int cyc_num = table.get<int>("Cycle No.", i);
+      double ce = table.get<double>("CE (Fractional)", i);
+      series->append(cyc_num, ce);
+      max_cyc_num = (max_cyc_num < cyc_num) ? cyc_num : max_cyc_num;
+      max_ce = (max_ce < ce) ? ce : max_ce;
     }
     chart->addSeries(series);
   }
-  chart->createDefaultAxes();
+  QValueAxis* xAx = createAxisWhole(0, max_cyc_num);
+  QValueAxis* yAx = createAxisDouble(0.5, max_ce, 11);
+  chart->setAxisX(xAx);
+  chart->setAxisY(yAx);
+  for (auto s : chart->series()) {
+    s->attachAxis(xAx);
+    s->attachAxis(yAx);
+  }
   QChartView* chartView = new QChartView(chart);
   chartView->setRenderHint(QPainter::Antialiasing);
   return chartView;
