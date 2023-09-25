6079d8c1f1691330140bf1e4bbb41e696b6f80c4
daitenshionyan <daitenshionyan@gmail.com>
2023-09-25 21:43:26 +0800

    Add documentation

--- library/vrfblib/include/public/vrfbcalc.hpp
+++ library/vrfblib/include/public/vrfbcalc.hpp
@@ -7,34 +7,37 @@
 #include "utillib/utils.hpp"
 
 
-namespace vrfb { // BEGIN OF NAMESPACE <vrfb> ==================================
+namespace vrfb { // ==== namespace <vrfb> ======================================
 
 
-namespace celleff { // BEGIN OF NAMESPACE <vrfb::celleff> ======================
+namespace celleff { // ==== namespace <vrfb::celleff> ==========================
 
 
-constexpr std::string_view kCycleNumberHdr = "Cycle No.";
-constexpr std::string_view kTotalTimeHdr = "Total Time (s)";
-constexpr std::string_view kCycleTimeHdr = "Cycle Time (s)";
-constexpr std::string_view kChgTimeHdr = "Chg Step Time (s)";
-constexpr std::string_view kDChgTimeHdr = "DChg Step Time (s)";
-constexpr std::string_view kChgCurrentHdr = "Chg Current (A)";
-constexpr std::string_view kDChgCurrentHdr = "DChg Current (A)";
-constexpr std::string_view kChgDensityHdr = "Chg Charge Density (A cm-2)";
-constexpr std::string_view kDChgDensityHdr = "DChg Charge Density (A cm-2)";
-constexpr std::string_view kChgVoltageHdr = "Chg Voltage (V)";
-constexpr std::string_view kDChgVoltageHdr = "DChg Voltage (V)";
-constexpr std::string_view kChgCapHdr = "Chg Capacity (Ah)";
-constexpr std::string_view kDChgCapHdr = "DChg Capacity (Ah)";
-constexpr std::string_view kChgEnergyHdr = "Chg Energy (Wh)";
-constexpr std::string_view kDChgEnergyHdr = "DChg Energy (Wh)";
-constexpr std::string_view kCEHdr = "CE (Fractional)";
-constexpr std::string_view kEEHdr = "EE (Fractional)";
-constexpr std::string_view kVEHdr = "VE (Fractional)";
-constexpr std::string_view kASRHdr = "ASR (Ohm cm-2)";
+// :::: [ Output table headers ] :::::::::::::::::::::::::::::::::::::::::::::::
 
 
-/* Output cell cycle headers. */
+constexpr std::string_view kCycleNumberHdr     = "Cycle No.";
+constexpr std::string_view kTotalTimeHdr       = "Total Time (s)";
+constexpr std::string_view kCycleTimeHdr       = "Cycle Time (s)";
+constexpr std::string_view kChgTimeHdr         = "Chg Step Time (s)";
+constexpr std::string_view kDChgTimeHdr        = "DChg Step Time (s)";
+constexpr std::string_view kChgCurrentHdr      = "Chg Current (A)";
+constexpr std::string_view kDChgCurrentHdr     = "DChg Current (A)";
+constexpr std::string_view kChgDensityHdr      = "Chg Charge Density (A cm-2)";
+constexpr std::string_view kDChgDensityHdr     = "DChg Charge Density (A cm-2)";
+constexpr std::string_view kChgVoltageHdr      = "Chg Voltage (V)";
+constexpr std::string_view kDChgVoltageHdr     = "DChg Voltage (V)";
+constexpr std::string_view kChgCapHdr          = "Chg Capacity (Ah)";
+constexpr std::string_view kDChgCapHdr         = "DChg Capacity (Ah)";
+constexpr std::string_view kChgEnergyHdr       = "Chg Energy (Wh)";
+constexpr std::string_view kDChgEnergyHdr      = "DChg Energy (Wh)";
+constexpr std::string_view kCEHdr              = "CE (Fractional)";
+constexpr std::string_view kEEHdr              = "EE (Fractional)";
+constexpr std::string_view kVEHdr              = "VE (Fractional)";
+constexpr std::string_view kASRHdr             = "ASR (cm2 Ohm)";
+
+
+/** Output cell cycle headers. */
 const std::vector<std::string> kCycleTableHdrs {
   std::string(kCycleNumberHdr),
   std::string(kTotalTimeHdr),
@@ -58,7 +61,12 @@ const std::vector<std::string> kCycleTableHdrs {
 };
 
 
-/* Structure containing cell efficiency calculation configuration. */
+// :::: [ Data structures ] ::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+/**
+ * Structure containing cell efficiency calculation configuration.
+*/
 struct Config {
   std::string t_time_h;                           /* Total time header */
   std::string type_h;                             /* Step type header */
@@ -72,50 +80,65 @@ struct Config {
 };
 
 
+/**
+ * Structure containing the raw cycling data as a `comutils::Table` and its
+ * associated configuration.
+*/
 struct Data {
   comutils::Table table;
   Config cfg;
 };
 
 
-/*
-  Calculates the cell efficiency performance of a cell.
+// :::: [ Functions ] ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-  @param datas List of data to process.
+
+/**
+ * Calculates the performance of a cell.
+ *
+ * @param area The active area of the cell in cm2.
+ * @param datas List of `vrfb::celleff::Data` representing the set of logged
+ *    data from the same cell cycling experiment.
+ * @returns The calculated performance of the cell as a `comutils::Table`. Its
+ *    headers are in the order given by `vrfb::celleff::kCycleTableHdrs`.
 */
 comutils::Table calcPerf(const double area, const std::vector<Data>& datas);
 
 
-} // END OF NAMESPACE <vrfb::celleff> ------------------------------------------
+} // ---- namepsace <vrfb::celleff>
 // namespace <vrfb>
 
 
-namespace shuntcur { // BEGIN OF NAMESPACE <vrfb::shuntcur> ====================
+namespace shuntcur { // ==== namespace <vrfb::shuntcur> ========================
 
 
-constexpr std::string_view kCellNumHdr = "Cell No.";
-
-constexpr std::string_view kCellCurHdr = "Cell Current (A)";
-constexpr std::string_view kShuntPosTopCurHdr = "SPT Current (A)";
-constexpr std::string_view kShuntPosBotCurHdr = "SPB Current (A)";
-constexpr std::string_view kShuntNegTopCurHdr = "SNT Current (A)";
-constexpr std::string_view kShuntNegBotCurHdr = "SNB Current (A)";
-constexpr std::string_view kManiPosTopCurHdr = "MPT Current (A)";
-constexpr std::string_view kManiPosBotCurHdr = "MPB Current (A)";
-constexpr std::string_view kManiNegTopCurHdr = "MNT Current (A)";
-constexpr std::string_view kManiNegBotCurHdr = "MNB Current (A)";
-
-constexpr std::string_view kCellPowHdr = "Cell Power (W)";
-constexpr std::string_view kShuntPosTopPowHdr = "SPT Power (W)";
-constexpr std::string_view kShuntPosBotPowHdr = "SPB Power (W)";
-constexpr std::string_view kShuntNegTopPowHdr = "SNT Power (W)";
-constexpr std::string_view kShuntNegBotPowHdr = "SNB Power (W)";
-constexpr std::string_view kManiPosTopPowHdr = "MPT Power (W)";
-constexpr std::string_view kManiPosBotPowHdr = "MPB Power (W)";
-constexpr std::string_view kManiNegTopPowHdr = "MNT Power (W)";
-constexpr std::string_view kManiNegBotPowHdr = "MNB Power (W)";
+// :::: [ Output table headers ] :::::::::::::::::::::::::::::::::::::::::::::::
 
 
+constexpr std::string_view kCellNumHdr         = "Cell No.";
+
+constexpr std::string_view kCellCurHdr         = "Cell Current (A)";
+constexpr std::string_view kShuntPosTopCurHdr  = "SPT Current (A)";
+constexpr std::string_view kShuntPosBotCurHdr  = "SPB Current (A)";
+constexpr std::string_view kShuntNegTopCurHdr  = "SNT Current (A)";
+constexpr std::string_view kShuntNegBotCurHdr  = "SNB Current (A)";
+constexpr std::string_view kManiPosTopCurHdr   = "MPT Current (A)";
+constexpr std::string_view kManiPosBotCurHdr   = "MPB Current (A)";
+constexpr std::string_view kManiNegTopCurHdr   = "MNT Current (A)";
+constexpr std::string_view kManiNegBotCurHdr   = "MNB Current (A)";
+
+constexpr std::string_view kCellPowHdr         = "Cell Power (W)";
+constexpr std::string_view kShuntPosTopPowHdr  = "SPT Power (W)";
+constexpr std::string_view kShuntPosBotPowHdr  = "SPB Power (W)";
+constexpr std::string_view kShuntNegTopPowHdr  = "SNT Power (W)";
+constexpr std::string_view kShuntNegBotPowHdr  = "SNB Power (W)";
+constexpr std::string_view kManiPosTopPowHdr   = "MPT Power (W)";
+constexpr std::string_view kManiPosBotPowHdr   = "MPB Power (W)";
+constexpr std::string_view kManiNegTopPowHdr   = "MNT Power (W)";
+constexpr std::string_view kManiNegBotPowHdr   = "MNB Power (W)";
+
+
+/** Output shunt current calculation headers. */
 const std::vector<std::string> kShuntLossTableHdrs {
   std::string(kCellNumHdr),
 
@@ -141,6 +164,12 @@ const std::vector<std::string> kShuntLossTableHdrs {
 };
 
 
+// :::: [ Data structures and classes ] ::::::::::::::::::::::::::::::::::::::::
+
+
+/**
+ * Structure contianing electric resistance configuration of a system.
+*/
 struct ResistConfig {
   std::string asrHdr;
 
@@ -162,32 +191,43 @@ struct ResistConfig {
   std::string maniAreaNegTopHdr;
   std::string maniAreaNegBotHdr;
 
-  double resistivity;
-  double area;
+  double resistivity;                 // Resistivity of electrolyte (Ohms m-1)
+  double area;                        // Active area (m2)
 };
 
 
+/** Class that manages the parameters of a cell stack system. */
 class SystemParam;
 
 
+/**
+ * Structure contianing the parameters of the stacks in a system.
+*/
 struct StackParam {
   std::size_t numCells;
-  double asr;
-  double cellArea;
-  double shuntLen;
-  double shuntArea;
-  double maniLen;
-  double maniArea;
-  double resistivity;
+  double asr;             // ASR (m2 Ohm)
+  double cellArea;        // Active area (m2)
+  double shuntLen;        // Shunt length (m)
+  double shuntArea;       // Shunt area (m2)
+  double maniLen;         // Manifold length (m)
+  double maniArea;        // Manifold area (m2)
+  double resistivity;     // Electrolyte resistivity (Ohm m-1)
 };
 
 
+/**
+ * A generator to generate `SystemParam` for shunt current calculations.
+*/
 class ParamGenerator {
   public:
     ParamGenerator(StackParam param_s)
         : stackParam{param_s} {}
 
     virtual ~ParamGenerator() = default;
+
+    /**
+     * Generate a `SystemParam`.
+    */
     virtual SystemParam generate() const = 0;
 
 
@@ -196,6 +236,10 @@ class ParamGenerator {
 };
 
 
+/**
+ * Implementation of `ParamGenerator` to generate `SystemParam` for systems
+ * with electrolyte flow to stacks connected in a stack arrangement.
+*/
 class StackArrGenerator : public ParamGenerator {
   public:
     StackArrGenerator(
@@ -216,10 +260,20 @@ class StackArrGenerator : public ParamGenerator {
 };
 
 
+// :::: [ Functions ] ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+/**
+ * Calculates the shunt current performance of a system.
+ *
+ * @param chgVolt The charging voltage (V).
+ * @param gen The generator to generate `SystemParam`
+ * @returns The calculated performance of the system as a `comutils::Table`.
+ *    Its headers are in the order given by
+ *    `vrfb::shuntcur::kShuntLossTableHdrs`.
+*/
 comutils::Table calcPerf(const double chgVolt, const ParamGenerator& gen);
 
 
-} // END OF NAMESPACE <vrfb::shuntcur> -----------------------------------------
-
-
-}
+} // ---- namespace <vrfb::shuntcur>
+} // ---- namespace <vrfb>
